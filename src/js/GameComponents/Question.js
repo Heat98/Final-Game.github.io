e464a2dda6688cc5655626816a4be668fbcd98f1
Heat98 <heeeat98@gmail.com>
2018-12-30 12:24:19 +0300

    refactor: refactor Question class

--- src/js/GameComponents/Question.js
+++ src/js/GameComponents/Question.js
@@ -1,17 +1,20 @@
 import questions from './questions'
 import CONSTANTS from '../Constants/constants';
 import questionsList from './questionsList'
+import jutsu from './Jutsu'
 
-export default class Question{
+export class Question{
     constructor() {
         this.iterator = questionsList[Symbol.iterator]();
         this.nextQuestion = this.iterator.next();
+        this.callback = () => jutsu.showJutsuMenu();
     }
 
     showQuestion() {
         this.ask();
-                CONSTANTS.questions.classList.remove('hide_element');
-                CONSTANTS.questions.classList.add('questions');
+        CONSTANTS.questions.classList.remove('hide_element');
+        CONSTANTS.questions.classList.add('questions');
+        CONSTANTS.answer.addEventListener('click', () => this.checkAnswer(this.callback));
     }
 
     hideQuestions() {
@@ -30,5 +33,9 @@ export default class Question{
             this.nextQuestion = this.iterator.next();
             callback();
         }
+    };
+
     }
-}
+
+const question = new Question();
+export default question;
