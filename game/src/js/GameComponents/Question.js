eac142f59d9a829f07e3db76bf43c449dd3af684
Heat98 <heeeat98@gmail.com>
2019-01-07 00:38:28 +0300

    feat: some trial features

--- game/src/js/GameComponents/Question.js
+++ game/src/js/GameComponents/Question.js
@@ -2,6 +2,8 @@ import questions from './questions';
 import CONSTANTS from '../Constants/constants';
 import questionsList from './questionsList';
 import jutsu from './Jutsu';
+import sasuke from './Sasuke';
+import naruto from './Naruto';
 
 export class Question{
     constructor() {
@@ -11,9 +13,9 @@ export class Question{
     }
 
     showQuestion() {
-        this.ask();
         CONSTANTS.questions.classList.remove('hide_element');
         CONSTANTS.questions.classList.add('questions');
+        CONSTANTS.question.innerHTML = this.nextQuestion.value[0];
         CONSTANTS.answer.addEventListener('click', () => this.checkAnswer(this.callback));
     }
 
@@ -22,16 +24,22 @@ export class Question{
         CONSTANTS.questions.classList.add('hide_element');
     }
 
-    ask() {
-        CONSTANTS.question.innerHTML = this.nextQuestion.value[0];
-    }
-
-
     checkAnswer(callback) {
         if (CONSTANTS.input.value === this.nextQuestion.value[1]) {
+            sasuke.chidori(100, 300);
+            CONSTANTS.narutoHealth.value -= 25;
             this.hideQuestions();
             this.nextQuestion = this.iterator.next();
+            console.log('yes');
             callback();
+        } else {
+            CONSTANTS.sasukeHealth.value -= 25;
+            console.log('no');
+            this.hideQuestions();
+            naruto.rassengan(900, 300);
+            setTimeout(() => {
+                callback();
+            },3000);
         }
     };
 
